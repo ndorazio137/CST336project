9b949e9b93cf2316b606e7b64b4fe8f92ee97f56
EC2 Default User <ec2-user@ip-172-31-33-152.us-east-2.compute.internal>
2020-07-29 08:33:45 +0000

    adding potions page

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", function(req, res) {
     res.render("index.ejs");
 });
 
+app.get("/potions", function(req, res) {
+    res.render("potions.ejs");
+});
+
 // starting server
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Express server is running...");
