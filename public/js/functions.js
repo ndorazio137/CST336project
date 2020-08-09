e4843b672d177d570d1d13894ce1f4c7bd342d48
Nick <ndorazio@csumb.edu>
2020-08-09 09:00:12 +0000

    Started implementing the admin login, and in the middle of trying to load content from JS. Some page rearrangement may be needed, and the footer is being shown in the wrong place

--- public/js/functions.js
+++ public/js/functions.js
@@ -2,24 +2,71 @@ $(document).ready(function() {
 
    // some data structure holders
 
-   // cart
+   //create cart. Map for constant lookup and deletion, plus iteration on keys. 
+   var cart = new Map();
+
 
    //push database product values to data structure holders
 
    //send form with hidden values to site html div
 
-   // map price to cart 
+   //create form to dynamically fill sales grid divs //TO BE TESTED
+   function createProduct(image, name, price, id) {
+      //<div class="box blue-box product-container">
+      let div = document.createElement("div");
+      div.setAttribute("class", "box blue-box product-container");
 
-   // empty cart
+      //<form></form>
+      let form = document.createElement("form");
 
-   //delete item from cart
+      //<img src=image> 
+      let productImage = document.createElement('img');
+      productImage.src = image;
 
-   //add item to cart
+      //<h5>Product Name</h5>
+      let productName = document.createElement("h5");
+      productName.value = name;
 
+      //<p>Price</p>
+      let priceTag = document.createElement("p");
+      priceTag.value = price.toString() + "Denar";
 
+      //<input type="hidden" name="product_id" value=id>
+      let hiddenId = document.createElement("input");
+      hiddenId.setAttribute("type", "hidden");
+      hiddenId.setAttribute("name", "product_id");
+      hiddenId.setAttribute("value", id.toString());
 
-   //form validation
+      //<input type="hidden" name="product_name" value=name>
+      let hiddenName = document.createElement("input");
+      hiddenName.setAttribute("type", "hidden");
+      hiddenName.setAttribute("name", "product_name");
+      hiddenName.setAttribute("value", name.toString());
 
+      //<input type="hidden" name="product_price" value=price>
+      let hiddenPrice = document.createElement("input");
+      hiddenPrice.setAttribute("type", "hidden");
+      hiddenPrice.setAttribute("name", "product_price");
+      hiddenPrice.setAttribute("value", price.toString());
+
+      //<input class="add-to-cart" type="submit" value="Add To Cart">
+      let submitButton = document.createElement("input");
+      submitButton.setAttribute("class", "add-to-cart");
+      submitButton.setAttribute("type", "submit");
+      submitButton.setAttribute("value", "Add To Cart");
+
+      form.appendChild(productImage);
+      form.appendChild(productName);
+      form.appendChild(priceTag);
+      form.appendChild(hiddenId);
+      form.appendChild(hiddenName);
+      form.appendChild(hiddenPrice);
+      form.appendChild(submitButton);
+
+      div.appendChild(form);
+
+      return div;
+   }
 
 
 });
