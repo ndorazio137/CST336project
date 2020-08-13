70fd2d7868b78c3e3ec0f4aeb689b1d6e27bcbd4
Nick <ndorazio@csumb.edu>
2020-08-13 13:03:14 +0000

    rendering addProduct path to resolve page hanging issue

--- app.js
+++ app.js
@@ -203,7 +203,7 @@ app.get("/api/addToCart", function(req, res) {
    console.log("Product name: " + req.query.product_name);
    console.log("Product price: " + req.query.product_price);
 
-   let sql = "";
+   let sql = "SELECT * FROM Products WHERE productId = ? AND name = ? AND price = ?";
    let sqlParams = [req.query.product_id, req.query.product_name, req.query.product_price];
    pool.query(sql, sqlParams, function(err, rows, fields) {
       if (err) throw err;
@@ -220,10 +220,9 @@ app.post("/api/addProduct", function(req, res) {
       // Render search results page, passing the results of the SQL query
       console.log(rows);
       console.log(sqlParams);
-      //res.render("searchResults", { "rows": rows });
+      res.render("admin", { "rows": rows });
    });
 });
-
 
 app.get("/search", function(req, res) {
 
--- public/js/functions.js
+++ public/js/functions.js
@@ -22,33 +22,4 @@ $(document).ready(function() {
             }
         }); //ajax
     });
-
-    $("#addProduct").on("submit", function(event) {
-        event.preventDefault();
-
-        let queryString = window.location.search;
-        let urlParams = new URLSearchParams(queryString);
-        let product_name = urlParams.get("product_name");
-        let product_category = urlParams.get("product_category");
-        let product_price = urlParams.get("product_price");
-        let product_description = urlParams.get("product_description");
-        let product_image = urlParams.get("product_image");
-        let product_quantity = urlParams.get("product_quantity");
-
-        $.ajax({
-            method: "POST",
-            url: "/api/addProduct",
-            data: {
-                "product_name": product_name,
-                "product_category": product_category,
-                "product_price": product_price,
-                "product_description": product_description,
-                "product_image": product_image,
-                "product_quantity": product_quantity
-            },
-            success: function(data, status) {
-                console.log("Returned from addProduct AJAX");
-            }
-        });
-    });
 });
